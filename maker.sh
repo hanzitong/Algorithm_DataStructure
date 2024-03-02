4c7842d09b404b4adc298222d624656240802154
Han Zitong <han.zitong@rm.is.tohoku.ac.jp>
2024-03-03 03:37:51 +0900

    rename directory

--- maker.sh
+++ maker.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-# NUMS="1 2 3 4 5 6 7 8 9 10 11 12"
-NUMS=${seq 1 12}
+NUMS="01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18"
+# NUMS=${seq 1 18}
 
 for NUM in $NUMS
 do
+    mkdir Chapter_${NUM}
     cd Chapter_${NUM}
     touch Chapter_${NUM}_Summary
     # rm Chapter_${NUM}_Summary
